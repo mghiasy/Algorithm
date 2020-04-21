91100ff67c4547ae3d0bb188779736d9372948e2
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-21 17:46:21 -0500

    Add find element Class

--- Algorithms/Problems/src/main/java/FindElement.java
+++ Algorithms/Problems/src/main/java/FindElement.java
@@ -1,6 +1,18 @@
 public class FindElement {
     //Suppose the array arr is already known to be in sorted order, Determine whether z is in arr
-    public static boolean find(int[] arr, int z) { //implement
-        return true;
+    public static boolean find(int[] arr, int z) {
+        //implement
+        for(int i=0;i<arr.length;i++){
+            System.out.println(i);
+            if(arr[i]>z){
+                return false;
+            }
+            else{
+                if(arr[i]==z){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
