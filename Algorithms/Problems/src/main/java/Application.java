75c70c168dadda7fadf259fc91cbc7200313479b
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-21 17:48:10 -0500

    Fix project

--- Algorithms/Problems/src/main/java/Application.java
+++ Algorithms/Problems/src/main/java/Application.java
@@ -6,23 +6,15 @@ public class Application {
 //        for (int i = 0; i < 20; ++i) {
 //            System.out.println(generate(i));
 //        }
+
+        int[] array ={2,9,4,6,12,3,14};
+        System.out.println("SumFound result: " +SumFound.sumFound(array,10));
+        List<Integer> listArray = Arrays.asList(3, 9, 4, 6, 12, 2, 14);
+        System.out.println("SumFound result list: " +SumFound.sumFound(listArray,10));
+
+        System.out.println("Second smallest result : " +SecondSmallest.secondSmallest(array));
+        int[] sortedArray ={2,3,6,12,18,20};
+        System.out.println("FindElement result : " +FindElement.find(sortedArray,11));
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
