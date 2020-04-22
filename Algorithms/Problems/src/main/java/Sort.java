72121c50a824438cb13a93b3fd82dfded7d5418a
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-22 00:11:41 -0500

    Add sort method

--- Algorithms/Problems/src/main/java/Sort.java
+++ Algorithms/Problems/src/main/java/Sort.java
@@ -2,10 +2,22 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Sort {
-    public static void sort(ArrayList<Integer> list) { //implement
+    public static void sort(ArrayList<Integer> list) {
+        //implement
+        for(int i=0;i<list.size()-1;i++){
+            for(int j=i+1;j<list.size()-1;j++){
+                if(list.get(i)>list.get(j)) {
+                    int temp = list.get(i);
+                    list.set(i,list.get(j));
+                    list.set(j,temp);
+                }
+            }
+        }
     }
 
     //sorting is to be done in-place
-    public static void sort(LinkedList<Integer> list) { //implement
+    public static void sort(LinkedList<Integer> list) {
+        //implement
+        
     }
 }
