c1e02cdd410cfea227866cf216f1d3f2593a21af
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-24 23:03:57 -0500

    Add merge method

--- Algorithms/Basic problems/Problems/src/main/java/Merge.java
+++ Algorithms/Basic problems/Problems/src/main/java/Merge.java
@@ -6,22 +6,29 @@ public class Merge {
         List<Integer> newArr = new ArrayList<>();
         int counter1=0,  counter2=0;
         int size=arr2.length;
-            while (counter1 < arr1.length && counter2<arr2.length){
-                if(arr1[counter1] > arr2[counter2]){
-                    newArr.add(arr2[counter2]);
-                    counter2++;
-                }
-                else if(arr1[counter1] < arr2[counter2]){
-                    newArr.add(arr1[counter1]);
-                    counter1++;
-                }
-                else if(arr1[counter1] == arr2[counter2]){
-                    newArr.add(arr2[counter2]);
-                    newArr.add(arr1[counter1]);
-                    counter1++;
-                    counter2++;
-                }
+        int temp=0;
+        //So, it seems that in most cases, the pre-increment operator is better than,
+        // or equal to, the post-increment operator,
+        // as long as you do not need the previous value of whatever you are incrementing.
+//        for(int i=1;i<10;++i){
+//            temp = i;
+//        }
+        while (counter1 < arr1.length && counter2<arr2.length){
+            if(arr1[counter1] > arr2[counter2]){
+                newArr.add(arr2[counter2]);
+                counter2++;
             }
+            else if(arr1[counter1] < arr2[counter2]){
+                newArr.add(arr1[counter1]);
+                counter1++;
+            }
+            else if(arr1[counter1] == arr2[counter2]){
+                newArr.add(arr2[counter2]);
+                newArr.add(arr1[counter1]);
+                counter1++;
+                counter2++;
+            }
+        }
         if(counter1<arr1.length-1) {
             for(int i=counter1;i<arr1.length;i++){
                 newArr.add(arr1[i]);
