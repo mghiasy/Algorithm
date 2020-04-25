d6ee31bd361c5f2c325d6bb85c0062ca9fc1c4c4
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-24 23:06:38 -0500

    Add Bubble sort method

--- Algorithms/SortMethods/src/main/java/sortroutines/BubbleSort.java
+++ Algorithms/SortMethods/src/main/java/sortroutines/BubbleSort.java
@@ -1,5 +1,19 @@
 package sortroutines;
 
 public class BubbleSort {
-
+    public int[] bubbleSort(int[] arr){
+        for(int i=0;i<arr.length;++i){
+            for(int j=0;j<arr.length-1;++j){
+                if(arr[i]>arr[j]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+        return arr;
+    }
+    public void swap(int[] arr,int i, int j){
+        int temp= arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
 }
