e9006f6235bb16223fbd3c908f1178d4a87ccbb5
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-27 01:11:57 -0500

    Add recursive methos for reverse array

--- Algorithms/Basic problems/Problems/src/main/java/Application.java
+++ Algorithms/Basic problems/Problems/src/main/java/Application.java
@@ -7,8 +7,8 @@ public class Application {
         System.out.println("App");
 
         int[] array ={2,9,4,6,12,3,14};
-        List<Integer> list= Arrays.asList(2,9,4,6,12,3);
-        System.out.println("Reversed result: " + ReverseArray.binReverse(list,0,array.length-1));
+        List<Integer> list= Arrays.asList(2,9,4,6,12,3,14,10);
+        System.out.println("Reversed result: " + ReverseArray.binReverse(list,0,list.size()-1));
 
     }
 }
--- Algorithms/Basic problems/Problems/src/main/java/ReverseArray.java
+++ Algorithms/Basic problems/Problems/src/main/java/ReverseArray.java
@@ -2,19 +2,17 @@ import java.util.List;
 
 public class ReverseArray {
     public static List<Integer> binReverse(List<Integer> arr, int lower, int upper){
-        if(lower==upper) return arr;
-        if (upper==lower+1) return reverse(arr,lower,upper);
-
-        int mid =(upper+lower)/2;
-        binReverse(arr,lower,mid-1);
-        binReverse(arr,mid+1,upper);
+        if(lower>=upper) return arr;
+        reverse(arr,lower,upper);
+        binReverse(arr,++lower,--upper);
         return arr;
     }
 
     private static List<Integer> reverse(List<Integer> arr,int lower,int upper) {
         int temp= arr.get(lower);
-        arr.set(arr.get(upper),lower);
-        arr.set(temp,upper);
+        int upValue=arr.get(upper);
+        arr.set(lower,arr.get(upper));
+        arr.set(upper,temp);
         return arr;
     }
 }
