31d2ee757055ad2eacb7ede97b2cb260691c1412
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-29 18:53:24 -0500

    Add Improved bubbleSort, for sorted => O(n)

--- Algorithms/SortMethods/src/main/java/sortroutines/Application.java
+++ Algorithms/SortMethods/src/main/java/sortroutines/Application.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         List<Integer> arr= Arrays.asList(1,5,3,6,4,19,18) ;
-        System.out.println("Sorted array :"+ BubbleSort.bubbleSort(arr));
+        System.out.println("Sorted array :"+ ImprovedBubbleSort.bubbleSort(arr));
         List<Integer> sortedArr= Arrays.asList(1,2,3,4,5,6,7);
-        System.out.println("Sorted array :"+ BubbleSort.bubbleSort(arr));
+        System.out.println("Sorted array :"+ ImprovedBubbleSort.bubbleSort(arr));
     }
 }
--- Algorithms/SortMethods/src/main/java/sortroutines/ImprovedBubbleSort.java
+++ Algorithms/SortMethods/src/main/java/sortroutines/ImprovedBubbleSort.java
@@ -2,15 +2,20 @@ package sortroutines;
 
 import java.util.List;
 
-public class BubbleSort {
+//If array is already sorted => O(n)
+
+public class ImprovedBubbleSort {
     public static List<Integer> bubbleSort(List<Integer> arr){
         for(int i=0;i<arr.size();++i){
-            //with every loop latest not sorted element will be sort
-            //since i last elements are sorted => no need to check them
+            //If it first lop nothing changed ==> break
+            boolean swapped = false;
             for(int j=0;j<arr.size()-i-1;++j){
                 if(arr.get(i)>arr.get(j)){
                     swap(arr,j,j+1);
+                    swapped= true;
                 }
+                if(swapped == false)
+                    break;
             }
         }
         return arr;
@@ -21,3 +26,4 @@ public class BubbleSort {
         arr.set(temp,j);
     }
 }
+
