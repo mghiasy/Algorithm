8066002050232239ff67b239eacf61567634c9e9
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-29 01:47:35 -0500

    Add reverse method for linked list

---
+++ Algorithms/DataStructures/src/main/java/LinkedListDS.java
@@ -0,0 +1,29 @@
+public class LinkedListDS {
+    //?
+    static Node head;
+    static class Node{
+        int data;
+        Node next;
+        // Constructor to create a new node
+        // Next is by default initialized
+        // as null
+        Node(int d) {
+            data = d;
+        }
+    }
+    public int reverseLinkedList(){
+        Node prev=null;
+        Node current= head;
+        Node next=null;
+        while (current.next != null){
+            next=current.next;
+            current.next=prev;
+            //keep current as prev then go forward
+            prev= current;
+            //go forward
+            current=next;
+
+        }
+    }
+
+}
