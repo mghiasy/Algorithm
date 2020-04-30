b1fda80b3b1c75d0066e24f504ba6abc07c8626e
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-04-29 20:13:25 -0500

    Modify Iterative isPrime method

--- Algorithms/Basic problems/Problems/src/main/java/IsPrime.java
+++ Algorithms/Basic problems/Problems/src/main/java/IsPrime.java
@@ -6,11 +6,10 @@ public class IsPrime {
         if(n % 2 == 0 || n ==1)
             return false;
         //dont count even numbers
-        //i*i <= n?
+        //every Divisor of n is smaller root of n => this loop goes n^1/2 times
         for(int i=3; i*i <= n; i=i+2){
             if(n%i ==0) return false;
         }
         return true;
-
     }
 }
