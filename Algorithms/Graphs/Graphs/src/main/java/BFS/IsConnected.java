e0bc1077900ce67183b21e4223ef8776742f0ad8
MaryamGhiasvand <m.ghiyasvand001@gmail.com>
2020-05-09 00:11:40 -0500

    Basic questions in graph min spanning tree- kruskal

--- Algorithms/Graphs/Graphs/src/main/java/BFS/IsConnected.java
+++ Algorithms/Graphs/Graphs/src/main/java/BFS/IsConnected.java
@@ -2,8 +2,7 @@ package BFS;
 
 import Graphs.Graph;
 
-public class IsConnected extends BreadthFirstSearch {
-	private int numComponents = 0;
+		private int numComponents = 0;
 	public IsConnected(Graph graph) {
 		super(graph);
 	}
